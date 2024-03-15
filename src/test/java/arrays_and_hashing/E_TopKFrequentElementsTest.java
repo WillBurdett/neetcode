fb92418a656100affcba85c5763f972d0e3599af
WillBurdett <williamburdett1995@gmail.com>
2024-03-15 21:31:57 +0000

    5. Top K Frequent Elements - tests added

---
+++ src/test/java/arrays_and_hashing/E_TopKFrequentElementsTest.java
@@ -0,0 +1,28 @@
+package arrays_and_hashing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class E_TopKFrequentElementsTest {
+
+  public final int[] TEST1_INPUT = new int[]{1,1,1,2,2,3};
+  public final int[] TEST1_EXPECTED = new int[]{1,2};
+  public final int TEST1_K = 2;
+
+  public final int[] TEST2_INPUT = new int[]{1};
+  public final int[] TEST2_EXPECTED = new int[]{1};
+  public final int TEST2_K = 1;
+
+  @Test
+  public void topKFrequent_TEST1_HappyPath() {
+    int[] actual = E_TopKFrequentElements.topKFrequent(TEST1_INPUT, TEST1_K);
+    assertArrayEquals(TEST1_EXPECTED, actual);
+  }
+
+  @Test
+  public void topKFrequent_TEST2_HappyPath() {
+    int[] actual = E_TopKFrequentElements.topKFrequent(TEST2_INPUT, TEST2_K);
+    assertArrayEquals(TEST2_EXPECTED, actual);
+  }
+}
