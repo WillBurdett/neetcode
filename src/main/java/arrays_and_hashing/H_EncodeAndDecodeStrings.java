1ee8032873df983b9d4ec6ff3a0ee88d9501e27b
WillBurdett <williamburdett1995@gmail.com>
2024-03-19 20:25:39 +0000

    8. Encode and Decode Strings - official solution

--- src/main/java/arrays_and_hashing/H_EncodeAndDecodeStrings.java
+++ src/main/java/arrays_and_hashing/H_EncodeAndDecodeStrings.java
@@ -2,17 +2,67 @@ package arrays_and_hashing;
 
 import java.util.ArrayList;
 import java.util.Base64;
+import java.util.LinkedList;
 import java.util.List;
 
 public class H_EncodeAndDecodeStrings {
 
+  // OFFICIAL SOLUTION
+  final char DELIMITER = '#';
+  // Encodes a list of strings to a single string.
+  public String encodeOfficial(List<String> strs) {
+    StringBuilder sb = new StringBuilder();
+    for(String str : strs){
+      sb.append(str.length());
+      sb.append(DELIMITER);
+      sb.append(str);
+    }
+    return sb.toString();
+  }
+
+  // Decodes a single string to a list of strings.
+  public List<String> decodeOfficial(String s) {
+    List<String> res = new LinkedList<>();
+    char[] arr = s.toCharArray();
+    // iterate through the entire encoded string
+    for(int i = 0; i < arr.length; i++){
+      // Find the string length we prefixed each string with, then assign that to 'sb'
+      StringBuilder sb = new StringBuilder();
+      while(arr[i] != DELIMITER){
+        // plus 'i' after getting the string length
+        sb.append(arr[i++]);
+      }
+      // plus 'i' again to move from the 'DELIMITER' index it'll be resting on
+      i++;
+
+      // Use 'sb' to find the number of characters we should iterate through
+      int numOfChars = Integer.valueOf(sb.toString());
+      // Calculate the index we want to copy characters up to
+      int end = i + numOfChars;
+      // Create new string builder to store our string that'll eventually be added to the 'res' (result) list
+      sb = new StringBuilder();
+      // Iterate while the index is less than the one we want to go up to...
+      while(i < end){
+        // ...append the char then plus 'i'
+        sb.append(arr[i++]);
+      }
+      // we must 'i--' as 'i++' will occur at the start of the next loop and move our index too far forward
+      i--;
+      // add our 'sb' to the resulting list
+      res.add(sb.toString());
+    }
+    return res;
+  }
+
+  // ORIGINAL SOLUTION
+  private final String DELIMIT = "#";
   // Encodes a list of strings to a single string.
   public String encode(List<String> strs) {
     String result = "";
     for (String s:
     strs) {
       String encodedString = Base64.getEncoder().encodeToString(s.getBytes());
-      result += encodedString + " ";
+      result += encodedString + DELIMIT;
     }
 
     return result;
@@ -20,7 +70,7 @@ public class H_EncodeAndDecodeStrings {
 
   // Decodes a single string to a list of strings.
   public List<String> decode(String s) {
-    String[] sAsList = s.split(" ");
+    String[] sAsList = s.split(DELIMIT);
     List<String> result = new ArrayList<>();
     for (String j:
         sAsList) {
@@ -28,7 +78,6 @@ public class H_EncodeAndDecodeStrings {
       String decodedString = new String(decodedBytes);
       result.add(decodedString);
     }
-
     return result;
   }
 
--- src/test/java/arrays_and_hashing/H_EncodeAndDecodeStringsTest.java
+++ src/test/java/arrays_and_hashing/H_EncodeAndDecodeStringsTest.java
@@ -7,13 +7,15 @@ import org.junit.Test;
 
 public class H_EncodeAndDecodeStringsTest {
 
+  private final String DELIMITER = "#";
+
   private String HELLO_WORLD = "hello world";
   private String BOB = "bob";
   private String WHATS_UP = "what's up";
 
-  private String ENCODED_HELLO_WORLD_WITH_SPACE = "aGVsbG8gd29ybGQ= ";
-  private String ENCODED_BOB_WITH_SPACE = "Ym9i ";
-  private String ENCODED_WHATS_UP_WITH_SPACE = "d2hhdCdzIHVw ";
+  private String ENCODED_HELLO_WORLD_WITH_SPACE = "aGVsbG8gd29ybGQ=" + DELIMITER;
+  private String ENCODED_BOB_WITH_SPACE = "Ym9i" + DELIMITER;
+  private String ENCODED_WHATS_UP_WITH_SPACE = "d2hhdCdzIHVw" + DELIMITER;
 
   private String ENCODED_STRING = ENCODED_HELLO_WORLD_WITH_SPACE + ENCODED_BOB_WITH_SPACE + ENCODED_WHATS_UP_WITH_SPACE;
   private List<String> DECODED_LIST = List.of(HELLO_WORLD, BOB, WHATS_UP);
