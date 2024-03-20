33b8128cb89e0e79f850b8d178f295a592f63caf
WillBurdett <williamburdett1995@gmail.com>
2024-03-20 11:46:33 +0000

    9. Longest Consecutive Sequence - offical solution added

--- src/main/java/arrays_and_hashing/I_LongestConsecutiveSequence.java
+++ src/main/java/arrays_and_hashing/I_LongestConsecutiveSequence.java
@@ -1,9 +1,37 @@
 package arrays_and_hashing;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class I_LongestConsecutiveSequence {
 
+  // official solution (slower)
+  public static int longestConsecutiveOfficial(int[] nums) {
+    Set<Integer> numSet = new HashSet<>();
+    int longest = 0;
+
+    for (Integer n:
+    nums) {
+      numSet.add(n);
+    }
+
+    for (Integer n:
+        numSet) {
+      // check if it's start of sequence i.e. has number to left
+      if (!numSet.contains(n - 1)){
+        int length = 0;
+          while (numSet.contains(n + length)){
+            length += 1;
+          }
+         longest = length > longest ? length : longest;
+      }
+    }
+
+    return longest;
+  }
+
+  // original solution (faster)
   public static int longestConsecutive(int[] nums) {
     Arrays.sort(nums);
     int currentSequence = 0;
--- src/test/java/arrays_and_hashing/I_LongestConsecutiveSequenceTest.java
+++ src/test/java/arrays_and_hashing/I_LongestConsecutiveSequenceTest.java
@@ -40,4 +40,36 @@ public class I_LongestConsecutiveSequenceTest {
     assertEquals(expected, actual);
   }
 
+  @Test
+  public void longestConsecutiveSequenceOfficial_HappyPath(){
+    int[] nums = {100,4,200,1,3,2};
+    int actual = undertest.longestConsecutiveOfficial(nums);
+    int expected = 4;
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void longestConsecutiveSequenceOfficial_startsWithZeros(){
+    int[] nums = {0,3,7,2,5,8,4,6,0,1};
+    int actual = undertest.longestConsecutiveOfficial(nums);
+    int expected = 9;
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void longestConsecutiveSequenceOfficial_emptyArray(){
+    int[] nums = {};
+    int actual = undertest.longestConsecutiveOfficial(nums);
+    int expected = 0;
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void longestConsecutiveSequenceOfficial_middleNumberDuplicate(){
+    int[] nums = {1,2,0,1};
+    int actual = undertest.longestConsecutiveOfficial(nums);
+    int expected = 3;
+    assertEquals(expected, actual);
+  }
+
 }
