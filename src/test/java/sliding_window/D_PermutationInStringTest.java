de7e2c37bf93025ffa84036fc27bac43fa764934
WillBurdett <williamburdett1995@gmail.com>
2024-04-02 20:32:09 +0100

    18. PermutationInString - solved

---
+++ src/test/java/sliding_window/D_PermutationInStringTest.java
@@ -0,0 +1,46 @@
+package sliding_window;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class D_PermutationInStringTest {
+
+  private final D_PermutationInString undertest = new D_PermutationInString();
+
+  @Test
+  public void checkInclusion_returnsTrue() {
+    String s1 = "ab";
+    String s2 = "eidbaooo";
+    boolean actual = undertest.checkInclusion(s1, s2);
+    boolean expected = true;
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void checkInclusion_returnsFalse() {
+    String s1 = "ab";
+    String s2 = "eidboaoo";
+    boolean actual = undertest.checkInclusion(s1, s2);
+    boolean expected = false;
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void checkInclusion_returnsFalse2() {
+    String s1 = "hello";
+    String s2 = "ooolleoooleh";
+    boolean actual = undertest.checkInclusion(s1, s2);
+    boolean expected = false;
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void checkInclusion_returnsFalse3() {
+    String s1 = "adc";
+    String s2 = "dcda";
+    boolean actual = undertest.checkInclusion(s1, s2);
+    boolean expected = true;
+    assertEquals(expected, actual);
+  }
+}
