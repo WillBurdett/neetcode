7c43e7fc178cf034b8fcbbf52dca8d732b773b7f
WillBurdett <williamburdett1995@gmail.com>
2024-05-29 10:56:11 +0100

    A_ValidParentheses tests added

---
+++ src/main/java/stack/A_ValidParentheses.java
@@ -0,0 +1,9 @@
+package stack;
+
+public class A_ValidParentheses {
+
+  public boolean isValid(String s) {
+    return false;
+  }
+
+}
---
+++ src/test/java/stack/A_ValidParenthesesTest.java
@@ -0,0 +1,34 @@
+package stack;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class A_ValidParenthesesTest {
+
+  private final A_ValidParentheses undertest = new A_ValidParentheses();
+
+  @Test
+  public void isValid_shortValidStringReturnsTrue() {
+    String s = "[]";
+    boolean actual = undertest.isValid(s);
+    boolean expected = true;
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void isValid_mediumValidStringReturnsTrue() {
+    String s = "([{}])";
+    boolean actual = undertest.isValid(s);
+    boolean expected = true;
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void isValid_mediumInvalidStringReturnsFalse() {
+    String s = "[(])";
+    boolean actual = undertest.isValid(s);
+    boolean expected = false;
+    assertEquals(expected, actual);
+  }
+}
