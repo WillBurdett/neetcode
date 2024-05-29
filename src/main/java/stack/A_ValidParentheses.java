3c2b344bbd35327fcaf40385b425143ba2a25a65
WillBurdett <williamburdett1995@gmail.com>
2024-05-29 11:16:53 +0100

    A_ValidParentheses - solved

--- src/main/java/stack/A_ValidParentheses.java
+++ src/main/java/stack/A_ValidParentheses.java
@@ -1,9 +1,31 @@
 package stack;
 
+import java.util.Stack;
+
 public class A_ValidParentheses {
 
   public boolean isValid(String s) {
-    return false;
+    Stack<Character> stack = new Stack<>();
+    for (int i = 0; i < s.length(); i++) {
+      Character c = s.charAt(i);
+      if (c == '(' || c == '{'|| c == '['){
+        stack.push(c);
+        continue;
+      }
+      if (c == ')' && (stack.isEmpty() || stack.peek() != '(')){
+        return false;
+      }
+      if (c == ']' && (stack.isEmpty() || stack.peek() != '[')){
+        return false;
+      }
+      if (c == '}' && (stack.isEmpty() || stack.peek() != '{')){
+        return false;
+      }
+      if (!stack.isEmpty()){
+        stack.pop();
+      }
+    }
+    return stack.isEmpty();
   }
 
 }
