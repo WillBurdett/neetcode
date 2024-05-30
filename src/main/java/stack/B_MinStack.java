6182521662b8929d243a25e7f81ccdb0081a54ef
WillBurdett <williamburdett1995@gmail.com>
2024-05-30 11:49:18 +0100

    B_MinStack - updated with official solution

--- src/main/java/stack/B_MinStack.java
+++ src/main/java/stack/B_MinStack.java
@@ -6,17 +6,25 @@ import java.util.List;
 public class B_MinStack {
 
   private List<Integer> list;
+  private List<Integer> minList;
 
   public B_MinStack() {
     this.list = new ArrayList<>();
+    this.minList = new ArrayList<>();
   }
 
   public void push(int val) {
     list.add(val);
+    if (minList.size() > 0){
+      minList.add(Math.min(val, minList.get(minList.size()-1)));
+    } else {
+      minList.add(val);
+    }
   }
 
   public void pop() {
     list.remove(list.size() - 1);
+    minList.remove(minList.size() - 1);
   }
 
   public int top() {
@@ -24,17 +32,7 @@ public class B_MinStack {
   }
 
   public int getMin() {
-    int min = 0;
-    for (int i = 0; i < list.size(); i++) {
-      if (i == 0){
-        min = list.get(i);
-        continue;
-      }
-      if (list.get(i) < min){
-        min = list.get(i);
-      }
-    }
-   return min;
+   return minList.get(minList.size() - 1);
   }
 
   public List<Integer> getStack(){
