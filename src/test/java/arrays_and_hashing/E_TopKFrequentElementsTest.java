6dad4e854600f7d18ab15294a65509c1944460a6
WillBurdett <williamburdett1995@gmail.com>
2024-09-05 20:10:55 +0100

    tests added for E_TopFrequent...

--- src/test/java/arrays_and_hashing/E_TopKFrequentElementsTest.java
+++ src/test/java/arrays_and_hashing/E_TopKFrequentElementsTest.java
@@ -14,15 +14,27 @@ public class E_TopKFrequentElementsTest {
   public final int[] TEST2_EXPECTED = {1};
   public final int TEST2_K = 1;
 
+  public final int[] TEST3_INPUT = {1, 2};
+  public final int[] TEST3_EXPECTED = {1, 2};
+  public final int TEST3_K = 2;
+
+  private E_TopKFrequentElements undertest = new E_TopKFrequentElements();
+
   @Test
   public void topKFrequent_TEST1_HappyPath() {
-    int[] actual = E_TopKFrequentElements.topKFrequent(TEST1_INPUT, TEST1_K);
+    int[] actual = undertest.topKFrequent(TEST1_INPUT, TEST1_K);
     assertArrayEquals(TEST1_EXPECTED, actual);
   }
 
   @Test
   public void topKFrequent_TEST2_HappyPath() {
-    int[] actual = E_TopKFrequentElements.topKFrequent(TEST2_INPUT, TEST2_K);
+    int[] actual = undertest.topKFrequent(TEST2_INPUT, TEST2_K);
     assertArrayEquals(TEST2_EXPECTED, actual);
+  }
+
+  @Test
+  public void topKFrequent_TEST3_HappyPath() {
+    int[] actual = undertest.topKFrequent(TEST3_INPUT, TEST3_K);
+    assertArrayEquals(TEST3_EXPECTED, actual);
   }
 }
