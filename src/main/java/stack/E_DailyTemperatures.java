47b83e567c0b025b2b54da3ffb8039a2fbe20643
WillBurdett <williamburdett1995@gmail.com>
2024-10-19 20:21:02 +0100

    solution added to daily temperatures

--- src/main/java/stack/E_DailyTemperatures.java
+++ src/main/java/stack/E_DailyTemperatures.java
@@ -1,9 +1,32 @@
 package stack;
 
+import java.util.Stack;
+
 public class E_DailyTemperatures {
 
   public int[] dailyTemperatures(int[] temperatures) {
-    return new int[]{};
+    // store temperature and index
+    Stack <int[]> stack = new Stack<>();
+
+    // placeholder for days
+    int[] res = new int[temperatures.length];
+
+    for (int i = 0; i < temperatures.length; i++) {
+      // current temperature placeholder - this is for ease of reading instead of temperature[i]
+      int t = temperatures[i];
+
+      // if stack is not empty AND the current temperature is more than the top of the stack
+      while (!stack.isEmpty() && t > stack.peek()[0]) {
+        // pop the top temperature off the stack
+        int[] popped = stack.pop();
+        int stackIndex = popped[1];
+
+        // calculate the number of days passed and add value to the popped temperatures index in the result
+        res[stackIndex] = (i - stackIndex);
+      }
+      stack.push(new int[]{t, i});
+    }
+    return res;
   }
 
 }
--- src/test/java/stack/E_DailyTemperaturesTest.java
+++ src/test/java/stack/E_DailyTemperaturesTest.java
@@ -11,10 +11,20 @@ public class E_DailyTemperaturesTest {
   PractisePad undertest = new PractisePad();
 
   @Test
-  public void dailyTemperatures_happyPath() {
+  public void dailyTemperatures_happyPath1() {
     int[] input = new int[]{30,38,30,36,35,40,28};
 
     int[] expected = new int[]{1,4,1,2,1,0,0};
+    int[] actual = undertest.dailyTemperatures(input);
+
+    assertArrayEquals(expected, actual);
+  }
+
+  @Test
+  public void dailyTemperatures_happyPath2() {
+    int[] input = new int[]{22,21,20};
+
+    int[] expected = new int[]{0,0,0};
     int[] actual = undertest.dailyTemperatures(input);
 
     assertArrayEquals(expected, actual);
