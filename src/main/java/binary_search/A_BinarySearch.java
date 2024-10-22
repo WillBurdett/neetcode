1fe5e378cd40f2e2741e772b818cfee0fb7aa0dd
WillBurdett <williamburdett1995@gmail.com>
2024-10-22 15:22:07 +0100

    binary search added

---
+++ src/main/java/binary_search/A_BinarySearch.java
@@ -0,0 +1,12 @@
+package binary_search;
+
+public class A_BinarySearch {
+  public int search(int[] nums, int target) {
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] == target) {
+        return i;
+      }
+    }
+    return -1;
+  }
+}
---
+++ src/test/java/binary_search/A_BinarySearchTest.java
@@ -0,0 +1,32 @@
+package binary_search;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class A_BinarySearchTest {
+
+  private final A_BinarySearch undertest = new A_BinarySearch();
+
+  @Test
+  public void search_happyPath1() {
+    final int target = 4;
+    final int[] nums = new int[]{-1,0,2,4,6,8};
+
+    final int expected = 3;
+    final int actual = undertest.search(nums, target);
+
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void search_happyPath2() {
+    final int target = 3;
+    final int[] nums = new int[]{-1,0,2,4,6,8};
+
+    final int expected = -1;
+    final int actual = undertest.search(nums, target);
+
+    assertEquals(expected, actual);
+  }
+}
