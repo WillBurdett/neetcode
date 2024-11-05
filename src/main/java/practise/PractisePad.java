1097e0e752c210194ba5b5f5ad3f5588f927cfdd
WillBurdett <williamburdett1995@gmail.com>
2024-11-05 18:15:58 +0000

    comments adjusted on tests

--- src/main/java/practise/PractisePad.java
+++ src/main/java/practise/PractisePad.java
@@ -3,54 +3,66 @@ package practise;
 public class PractisePad {
 
   /**
-   * You are given an array of distinct integers nums, sorted in ascending order, and an integer target.
+   * <h2><strong>Binary Search</strong></h2>
    *
-   * Implement a function to search for target within nums. If it exists, then return its index, otherwise, return -1.
+   * You are given an array of distinct integers `nums`, sorted in ascending order, and an integer `target`.
    *
-   * Your solution must run in O(logn) time.
+   * Implement a function to search for `target` within `nums`. If it exists, return its index; otherwise, return -1.
    *
-   * Example 1:
+   * Your solution must run in O(log n) time.
    *
+   * <p><strong>Example 1:</strong></p>
+   * <pre>
    * Input: nums = [-1,0,2,4,6,8], target = 4
-   *
    * Output: 3
-   * Example 2:
+   * </pre>
    *
+   * <p><strong>Example 2:</strong></p>
+   * <pre>
    * Input: nums = [-1,0,2,4,6,8], target = 3
-   *
    * Output: -1
-   * Constraints:
+   * </pre>
    *
-   * 1 <= nums.length <= 10000.
-   * -10000 < nums[i], target < 10000
-   **/
+   * <p><strong>Constraints:</strong></p>
+   * <ul>
+   *   <li>1 <= nums.length <= 10,000</li>
+   *   <li>-10,000 < nums[i], target < 10,000</li>
+   * </ul>
+   */
   public int search(int[] nums, int target) {
     return -1;
   }
 
   /**
-   * You are given an m x n 2-D integer array matrix and an integer target.
+   * <h2><strong>Binary Search a 2D Matrix</strong></h2>
    *
-   * Each row in matrix is sorted in non-decreasing order.
+   * You are given an m x n 2-D integer array `matrix` and an integer `target`.
+   *
+   * Each row in `matrix` is sorted in non-decreasing order.
    * The first integer of every row is greater than the last integer of the previous row.
-   * Return true if target exists within matrix or false otherwise.
+   * Return `true` if `target` exists within `matrix`, or `false` otherwise.
    *
-   * Can you write a solution that runs in O(log(m * n)) time?
+   * Your solution should run in O(log(m * n)) time.
    *
-   * Example 1:
+   * <p><strong>Example 1:</strong></p>
+   * <pre>
    * Input: matrix = [[1,2,4,8],[10,11,12,13],[14,20,30,40]], target = 10
    * Output: true
+   * </pre>
    *
-   *
-   * Example 2:
+   * <p><strong>Example 2:</strong></p>
+   * <pre>
    * Input: matrix = [[1,2,4,8],[10,11,12,13],[14,20,30,40]], target = 15
    * Output: false
+   * </pre>
    *
-   * Constraints:
-   * m == matrix.length
-   * n == matrix[i].length
-   * 1 <= m, n <= 100
-   * -10000 <= matrix[i][j], target <= 10000
+   * <p><strong>Constraints:</strong></p>
+   * <ul>
+   *   <li>m == matrix.length</li>
+   *   <li>n == matrix[i].length</li>
+   *   <li>1 <= m, n <= 100</li>
+   *   <li>-10,000 <= matrix[i][j], target <= 10,000</li>
+   * </ul>
    */
   public boolean searchMatrix(int[][] matrix, int target) {
     return false;
