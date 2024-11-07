2e66a1737bfa8468b5f2c9378813df8fc3eb6fc3
WillBurdett <williamburdett1995@gmail.com>
2024-11-07 10:18:55 +0000

    comments added to group anagrams

--- src/main/java/practise/PractisePad.java
+++ src/main/java/practise/PractisePad.java
@@ -17,6 +17,40 @@ public class PractisePad {
     return new int[]{};
   }
 
+  /**
+   * <h2><strong>Anagram Groups</strong></h2>
+   *
+   * Groups an array of strings into sublists of anagrams.
+   *
+   * <p>Given an array of strings {@code strs}, this method groups all anagrams together
+   * into sublists. Each sublist contains strings that are anagrams of each other.
+   * An anagram is defined as a string containing the same characters as another string,
+   * but in any order.
+   *
+   * <p>The output can be returned in any order.
+   *
+   * <p>Examples:
+   * <pre>
+   * Example 1:
+   * Input: strs = ["act","pots","tops","cat","stop","hat"]
+   * Output: [["hat"],["act", "cat"],["stop", "pots", "tops"]]
+   *
+   * Example 2:
+   * Input: strs = ["x"]
+   * Output: [["x"]]
+   *
+   * Example 3:
+   * Input: strs = [""]
+   * Output: [[""]]
+   * </pre>
+   *
+   * <p>Constraints:
+   * <ul>
+   *   <li>{@code 1 <= strs.length <= 1000}</li>
+   *   <li>{@code 0 <= strs[i].length <= 100}</li>
+   *   <li>Each {@code strs[i]} consists of lowercase English letters.</li>
+   * </ul>
+   */
   public List<List<String>> groupAnagrams(String[] strs) {
     return new ArrayList<>();
   }
