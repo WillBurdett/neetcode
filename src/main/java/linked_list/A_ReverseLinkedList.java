dd1a5bf3c4bdbc10b2629b2af9f8b2e00b1f72a4
WillBurdett <williamburdett1995@gmail.com>
2024-11-24 17:27:48 +0000

    annotations added to solution

--- src/main/java/linked_list/A_ReverseLinkedList.java
+++ src/main/java/linked_list/A_ReverseLinkedList.java
@@ -7,9 +7,16 @@ public class A_ReverseLinkedList {
     ListNode curr = head;
 
     while (curr != null) {
+      // storing the next node from the current one we're looking at
       ListNode temp = curr.next;
+
+      // set the current nodes previous field to the 'prev' node we saved above
       curr.next = prev;
+
+      // now we update the 'prev' variable equal to our updated current node
       prev = curr;
+
+      // we set the current node to the next one we stored earlier
       curr = temp;
     }
     return prev;
